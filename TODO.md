561a92dfa5bbf292969a9a00e00a02c64242708a
marlon symczecym <marlonsymczecym.dev@gmail.com>
2021-01-05 22:44:17 -0300

    add tests and module client to register and show clients

---
+++ TODO.md
@@ -0,0 +1,24 @@
+# Mini Ecommerce
+
+Project Description
+
+### Todo
+
+- Implementar atualizar cliente  
+- Implementar fazer compra de produtos  
+- Implementar validação no metodo register_product para não deixar registrar produtos com nomes iguais  
+
+### In Progress
+
+- Implementar deletar cliente  
+
+### Done ✓
+
+- Implementar ver um cliente  
+- Implementar ver todos os clientes  
+- Implementar ver todos os clientes  
+- Implementar cadastrar cliente  
+- Implementar Modulo Cliente com struct do cliente  
+- [x] Implementar Editar informações de um produto  
+- [x] Implementar Ver todos produtos  
+
---
+++ lib/client.ex
@@ -0,0 +1,106 @@
+defmodule Client do
+  @moduledoc """
+  Modulo Client faz relacao a conta de um cliente
+  """
+
+  @client_filepath "clients"
+  @client_file "clients.txt"
+  @client_path "#{@client_filepath}/#{@client_file}"
+
+  defstruct name: nil,
+            cpf: nil,
+            telephone: nil,
+            country: nil,
+            state: nil,
+            city: nil,
+            district: nil,
+            street: nil,
+            house_number: nil,
+            purchase: []
+
+  @doc """
+  Funcao faz o registro de um novo cliente na base de dados
+
+  ## Parametros da funcao
+
+  - name: nome do cliente
+  - cpf: numero do cpf
+  - tel: numero de telefone
+  - country: nome do País
+  - state: nome do estado
+  - city: nome da cidade
+  - district: nome do bairro
+  - street: nome da rua
+  - house_number: numero da casa
+  """
+  def register_client(name, cpf, tel, country, state, city, district, street, house_number) do
+    (read() ++
+       [
+         %__MODULE__{
+           name: name,
+           cpf: cpf,
+           telephone: tel,
+           country: country,
+           state: state,
+           city: city,
+           district: district,
+           street: street,
+           house_number: house_number
+         }
+       ])
+    |> :erlang.term_to_binary()
+    |> write()
+
+    {:ok, "Cliente #{name} registrado com sucesso"}
+  end
+
+  defp write(list_clients) do
+    File.write(@client_path, list_clients)
+  end
+
+  defp read() do
+    {:ok, file} = File.read(@client_path)
+
+    file
+    |> :erlang.binary_to_term()
+  end
+
+  @doc """
+  Funcao que mostra todos os clientes cadastrados na base de dados
+  """
+  def show_all() do
+    cond do
+      read() |> Enum.count() == 0 ->
+        {:error, "Sem clientes cadastrasdos"}
+
+      read() |> Enum.count() > 0 ->
+        read()
+        |> Enum.each(&echo/1)
+    end
+  end
+
+  def show_client(name, cpf) do
+    case read |> Enum.find(&(&1.name == name && &1.cpf == cpf)) do
+      nil ->
+        {:error, "Cliente nao encontrado"}
+
+      _ ->
+        read()
+        |> Enum.find(&(&1.name == name && &1.cpf == cpf))
+        |> echo()
+    end
+  end
+
+  defp echo(client) do
+    IO.puts("================")
+    IO.puts("Nome: #{client.name}")
+    IO.puts("CPF: #{client.cpf}")
+    IO.puts("Telefone: #{client.telephone}")
+    IO.puts("País: #{client.country}")
+    IO.puts("Estado: #{client.state}")
+    IO.puts("Cidade: #{client.city}")
+    IO.puts("Bairro: #{client.district}")
+    IO.puts("Rua: #{client.street}")
+    IO.puts("Numero da casa: #{client.house_number}")
+  end
+end
